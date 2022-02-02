e68f7f81af79913f20cdf5f6c0c4b5b20853862d
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-02 09:04:39 -0500

    Completed operators

--- js_ex-intro.js
+++ js_ex-intro.js
@@ -1,7 +1,6 @@
 "use strict";
 
 // PRIMITIVE TYPES
-
 // Identify the data type of the following values:
 //
 // 99.9        = number
@@ -31,27 +30,44 @@
 // * * *
 
 // OPERATORS
-
 // For each of the following code blocks, read the expression and predict what
 // the result of evaluating it would be, then execute the expression in the Chrome console.
 //
 // '1' + 2
-//      = '12'
+//      = 12
 // typeof '1' + 2
+//      = string2
 // 6 % 4
+//      = 2
 // '3 + 4 is ' + 3 + 4
+//      = 3 + 4 is 34
 // // how could you make this produce the correct output?
+//      = 3 + 4 is ' + (3 + 4)
 // 0 < 0
+//      = false
 // 'false' == false
+//      = false
 // true == 'true'
+//      = false
 // 5 >= -5
+//      = true
 // !false || false
+//      = true
 // true || "42"
+//      = true
 // !true && !false
+//      = false
 // 6 % 5
+//      = 1
 // 5 < 4 && 1 === 1
+//      = false
 // typeof 'codeup' === 'string'
+//      = false
 // 'codeup' === 'codeup' && 'codeup' === 'Codeup'
+//      = false
 // 4 >= 0 && 1 !== '1'
+//      = true
 // 6 % 3 === 0
+//      = true
 // 5 % 2 !== 0
+//      = true

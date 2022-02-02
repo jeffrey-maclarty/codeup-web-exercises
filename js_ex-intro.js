81380244742429691323219bb5e6acfa3043ee8a
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-01 20:56:19 -0500

    Initial

---
+++ js_ex-intro.js
@@ -0,0 +1,57 @@
+"use strict";
+
+// PRIMITIVE TYPES
+
+// Identify the data type of the following values:
+//
+// 99.9        = number
+// "false"     = string
+// false       = boolean
+// '0'         = string
+// 0           = number
+// true        = boolean
+// 'true'      = string
+// * * *
+
+// What data type would best represent:
+//
+// A term or phrase typed into a search box?
+//     string
+// If a user is logged in?
+//     boolean
+// A discount amount to apply to a user's shopping cart?
+//     number
+// Whether or not a coupon code is valid?
+//     boolean
+// An email address typed into a registration form?
+//     string
+// The price of a product?
+//     number
+
+// * * *
+
+// OPERATORS
+
+// For each of the following code blocks, read the expression and predict what
+// the result of evaluating it would be, then execute the expression in the Chrome console.
+//
+// '1' + 2
+//      = '12'
+// typeof '1' + 2
+// 6 % 4
+// '3 + 4 is ' + 3 + 4
+// // how could you make this produce the correct output?
+// 0 < 0
+// 'false' == false
+// true == 'true'
+// 5 >= -5
+// !false || false
+// true || "42"
+// !true && !false
+// 6 % 5
+// 5 < 4 && 1 === 1
+// typeof 'codeup' === 'string'
+// 'codeup' === 'codeup' && 'codeup' === 'Codeup'
+// 4 >= 0 && 1 !== '1'
+// 6 % 3 === 0
+// 5 % 2 !== 0

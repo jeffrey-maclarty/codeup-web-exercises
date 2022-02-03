fb60dcc406d9a75dd19211643c2d8e496ccdaebe
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-03 07:58:57 -0500

    Complete

--- js/external.js
+++ js/external.js
@@ -8,28 +8,20 @@ console.log();
 console.log("Hello from external JavaScript");
 console.log();
 
-console.log("------------");
-console.log("Exercise 1");
-console.log("------------");
+// EXERCISE 1
 // Use the alert function to show a message that says 'Welcome to my Website!'.
-
 alert("Welcome to my Website!");
 
-console.log("------------");
-console.log("Exercise 2");
-console.log("------------");
+// EXERCISE 2
 // Use a prompt to ask for the user's favorite color. Use the user's response to alert
 // a message that says that the color entered is your favorite color too.
 // For example, if the user enters "blue", your code should alert a message that says:
 // "Great, blue is my favorite color too!"
-
 let userColor = prompt("What is your favorite color?");
-console.log("Great, " + userColor + " is my favorite color too!");
+
 alert("Great, " + userColor + " is my favorite color too!");
 
-console.log("------------");
-console.log("Exercise 3");
-console.log("------------");
+// EXERCISE 3
 // Complete exercise 3 from the previous lesson, but write your code in the external.js file
 // instead of in the console.
 // When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
@@ -43,18 +35,46 @@ console.log("------------");
 // You have rented some movies for your kids: The little mermaid (for 3 days), Brother Bear (for 5 days,
 // they love it), and Hercules (1 day, you don't know yet if they're going to like it). If price for a
 // movie per day is $3, how much will you have to pay?
+let littleMermaid = 3;
+let brotherBear = 5;
+let hercules = 1;
+let dailyCost = 3;
+let totalPrice = (littleMermaid * dailyCost) + (brotherBear * dailyCost) + (hercules * dailyCost);
+
+alert("Total cost of movies is $" + totalPrice + ".");
 
 // 3b
 // Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you
 // a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you
 // receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours
 // for Amazon.
+let googleHourly = 400;
+let amazonHourly = 380;
+let facebookHourly = 350;
+let googleHours = 6;
+let amazonHours = 4;
+let facebookHours = 10;
+let totalPay = (googleHourly * googleHours) + (amazonHourly + amazonHours) + (facebookHourly + amazonHours);
+
+alert("Total contracting pay is $" + totalPay + ".");
 
 // 3c
 // A student can be enrolled in a class only if the class is not full and the class schedule does not
 // conflict with her current schedule.
+let classFull = confirm("Is the class full? ok = yes / cancel = no");
+let classConflict = confirm("Is there a scheduling conflit? ok = yes / cancel = no");
+let canEnroll = !classFull && !classConflict;
+
+alert("Student may enroll in class: " + canEnroll);
 
 // 3d
 // A product offer can be applied only if a person buys more than 2 items,
-// and the offer has not expired. Premium members do not need to buy a specific amount of products.
+// and the offer has not expired. Premium members do not need to buy a specific
+// amount of products.
+let numberOfItems = prompt("Number of items bought?");
+let offerValid = confirm("Offer is still valid? ok = yes / cancel = no");
+let premiumMember = confirm("Premium member? ok = yes / cancel = no");
+let mayPurchase = (numberOfItems > 2 && offerValid === true) || (offerValid === true && premiumMember === true);
+
+alert("Customer may purchase items: " + mayPurchase);
 

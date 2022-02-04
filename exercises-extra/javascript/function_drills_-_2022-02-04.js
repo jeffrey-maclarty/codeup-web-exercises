314463cb12c68c8587339c362fe4403318ebbbdb
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-04 17:13:42 -0500

    10 completed

--- exercises-extra/javascript/function_drills_-_2022-02-04.js
+++ exercises-extra/javascript/function_drills_-_2022-02-04.js
@@ -3,32 +3,32 @@ console.log("Link Test");
 console.log("---------");
 console.log();
 
-// Make a function named isOdd(number)
+// 1. Make a function named isOdd(number)
 function isOdd(number) {
     return number % 2 !== 0;
 }
 
-// Make a function named isEven(number)
+// 2. Make a function named isEven(number)
 function isEven(number) {
     return number % 2 === 0;
 }
 
-// Make a function named identity(input) that returns the input exactly as provided
+// 3. Make a function named identity(input) that returns the input exactly as provided
 function identity(input) {
     return input;
 }
 
-// Make a function named isFive(input)
+// 4. Make a function named isFive(input)
 function isFive(input) {
     return input === 5;
 }
 
-// Make a function named addFive(input) that adds five to some input
+// 5. Make a function named addFive(input) that adds five to some input
 function addFive(input) {
     return input + 5;
 }
 
-// Make a function named isMultipleOfFive(input)
+// 6. Make a function named isMultipleOfFive(input)
 function isMultipleOfFive(input) {
     let multFive = input / 5;
     if (multFive % 1 === 0) {
@@ -36,5 +36,37 @@ function isMultipleOfFive(input) {
     } else {
         return `${input} is _NOT_ a multiple of 5`;
     }
+}
+
+// 7. Make a function named isThree(input)
+function isThree(input) {
+    return input === 3;
+}
+
+// 8. Make a function named isMultipleOfThree(input)
+function isMultipleOfThree(input) {
+    let multThree = input / 3;
+    if (multThree % 1 === 0) {
+        return `${input} is a multiple of 3`;
+    } else {
+        return `${input} is _NOT_ a multiple of 3`;
+    }
+}
+
+// 9. Make a function named IsMultipleOfThreeAndFive(input)
+function isMultipleOfThreeAndFive(input) {
+    let multThree = input / 3;
+    let multFive = input / 5;
+    if (multThree % 1 === 0 && multFive % 1 === 0) {
+        return `${input} is a multiple of 3 and 5`;
+    } else {
+        return `${input} is _NOT_ a multiple of 3 and 5`;
+    }
+}
+
+// 10. Make a function named isMultipleOf(target, n) which checks if target is evenly divisible by n
+function isMultipleOf(target, n) {
+    let isMult = target / n;
+    return isMult % 1 === 0;
 }
 

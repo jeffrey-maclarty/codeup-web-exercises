acdfdaa2d5e0b46e6a3875da6fbd5928a78689f4
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-07 17:02:55 -0500

    Update

--- exercises-extra/javascript/math.random.js
+++ exercises-extra/javascript/math.random.js
@@ -5,5 +5,15 @@ console.log();
     
 console.log(Math.random());
 
-let randNum = Math.random();
-console.log(randNum);
+let randomNum = Math.random();
+console.log(`Math.random(); : ${randomNum}`);
+
+// random whole number 1 -10
+let randomWholeNum = Math.floor(Math.random() * 10) + 1;
+console.log(`( 1 - 10 ) Math.floor(Math.random() * 10) + 1; : ${randomWholeNum}`);
+
+let randomWholeNum2 = Math.floor(Math.random() * 10) + 10;
+console.log(`( 10 - 19 ) Math.floor(Math.random() * 10) + 10; : ${randomWholeNum2}`);
+
+let randomWholeNum3 = Math.floor(Math.random() * 10) + 11;
+console.log(`( 11 - 20 ) Math.floor(Math.random() * 10) + 11; : ${randomWholeNum3}`);

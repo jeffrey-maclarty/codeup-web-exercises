1d728ad97cbe0ca73a1dc32dce362dcf37139e48
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-07 17:53:10 -0500

    Update

--- js/for_loops.js
+++ js/for_loops.js
@@ -2,4 +2,10 @@
 console.log("Link Test");
 console.log("---------");
 console.log();
-    
+
+function showMultiplicationTable(num) {
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${num} x ${i} = ${num * i}`);
+        // console.log(num + " + " + i + " = " + (num * i))
+    }
+}

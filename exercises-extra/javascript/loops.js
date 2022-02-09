574cdfc564fd7416eea8a99b31908984fda44ead
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-09 15:35:29 -0500

    While loop practice

--- exercises-extra/javascript/loops.js
+++ exercises-extra/javascript/loops.js
@@ -5,7 +5,53 @@ console.log("         ");
 
 // URL
 
-console.log("------------");
-console.log("Exercise 1");
-console.log("------------");
 
+// var i = 0;
+//
+// while (i < 10) {
+//     i++;
+//     console.log("while loop iteration #" + i);
+    // i++;
+// }
+
+// let i = -3;
+//
+// do {
+//     console.log("while loop iteration #" + i);
+//     i++;
+// } while (i < 10);
+
+// let a = 1;
+//
+// while (a <= 5) {
+//     a++;
+//     console.log("a = " + a);
+//
+// }
+//
+// let b = 1;
+//
+// do {
+//     b++;
+//     console.log("b = " + b);
+// } while (a <= 5);
+
+
+// let isAwesome = true;
+// let incrementor = 1;
+// let decrementor = 24
+
+// if (true) {
+//     console.log("I am awesome!");
+// }
+
+// while (incrementor > 0) {
+//     console.log(decrementor--);
+// }
+
+// TODO: Use a while loop to output the numbers from 8-50
+let i = 8
+while (i <= 50) {
+    console.log(i)
+    i++;
+}

0c2ac0efe413d805f9691bfd960eb6d99d8099be
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-09 17:40:13 -0500

    Fixed negative cones

--- js/while.js
+++ js/while.js
@@ -28,14 +28,15 @@ console.log("------------");
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
 // This expression will generate a random number between 1 and 5
-Math.floor(Math.random() * 5) + 1;
+// Math.floor(Math.random() * 5) + 1;
 
 let purchase;
 
 console.log("Total cones to sell: " + allCones);
 
 do {
+    console.log("Client purchases: " + purchase + " cones.  There are " + allCones + " cones left.");
     purchase = Math.floor(Math.random() * 5) + 1;
     allCones = allCones - purchase;
-    console.log("Client purchases: " + purchase + " cones.  There are " + allCones + " cones left.");
-} while (allCones > purchase)
+
+} while (purchase < allCones);

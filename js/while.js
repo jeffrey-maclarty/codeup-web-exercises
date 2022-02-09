4e2d87f5a10e10d41f7d4874e8f0eb18c3cd4895
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-09 17:32:10 -0500

    While loops complete

--- js/while.js
+++ js/while.js
@@ -3,9 +3,41 @@ console.log("Link Test");
 console.log("---------");
 console.log("         ");
 
-// URL
+// https://java.codeup.com/javascript-i/loops/
+
+// console.log("------------");
+// console.log("Exercise 2");
+// console.log("------------");
+// let i = 2;
+//
+// while (i <= 65536) {
+//     console.log(i);
+//     i = i * 2;
+// }
 
 console.log("------------");
-console.log("Exercise 1");
+console.log("Exercise 3");
 console.log("------------");
+// An ice cream seller can't go home until she sells all of her cones. First' +
+// 'write enough code that generates a random number between 50 and 100' +
+// 'representing the amount of cones to sell before you start your loop. Inside' +
+// 'of the loop your code should generate another random number between 1 and 5,' +
+// 'simulating the amount of cones being bought by her clients. Use a do-while' +
+// 'loop to log to the console the amount of cones sold to each person. The below' +
+// 'code shows how to get the random numbers for this exercise.
+// This is how you get a random number between 50 and 100
+var allCones = Math.floor(Math.random() * 50) + 50;
+// This expression will generate a random number between 1 and 5
+Math.floor(Math.random() * 5) + 1;
 
+
+let conesLeftToSell;
+let purchase;
+
+console.log("Total cones to sell: " + allCones);
+
+do {
+    purchase = Math.floor(Math.random() * 5) + 1;
+    allCones = allCones - purchase;
+    console.log("Client purchases: " + purchase + " cones.  There are " + allCones + " cones left.");
+} while (allCones > purchase)

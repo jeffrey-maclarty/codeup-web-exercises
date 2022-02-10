4a2d01d7f74964f867b9a4de2e372349c9a6593e
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-10 12:42:37 -0500

    Changed to template string in addZero function

--- js/for_loops.js
+++ js/for_loops.js
@@ -136,7 +136,7 @@ console.log("----------");
 //  -> If it's neither print "I'm not playing"
 //  -> Print "marco polo" if it's divisible by 3 and 5.
 
-// marcoPolo();
+marcoPolo();
 
 function marcoPolo() {
 
@@ -157,7 +157,7 @@ function marcoPolo() {
 
         function addZero() {
             if (i.toString().length === 1) {
-                i = ("0" + i);
+                i = (`0${i}`)
             }
         }
     }

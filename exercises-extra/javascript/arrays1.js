140653ecefab1d7967cf39fba8f50e1a3ae4a176
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-09 20:21:12 -0500

    Update

--- exercises-extra/javascript/arrays1.js
+++ exercises-extra/javascript/arrays1.js
@@ -3,9 +3,28 @@ console.log("Link Test");
 console.log("---------");
 console.log("         ");
 
-// URL
+let schedule = ['wake up', 'breakfast', 'drive to work', 'work', 'drive home', 'dinner'];
 
-console.log("------------");
-console.log("Exercise 1");
-console.log("------------");
+let numbers = [132, 2535, 6232, 245];
 
+console.log(schedule);
+console.log(numbers);
+
+console.log(schedule[2]);
+console.log(numbers[3]);
+
+// ARRAY.push() - add item to end of array
+schedule.push('sleep');
+console.log(schedule);
+
+// ARRAY.pop() - remove last item in the array
+numbers.pop();
+console.log(numbers);
+
+// ARRAY.shift() - remove first item in the array
+schedule.shift();
+console.log(schedule);
+
+// ARRAY.indexOf(ITEM) - identify index of item
+let workIndex = schedule.indexOf('work');
+console.log(workIndex);

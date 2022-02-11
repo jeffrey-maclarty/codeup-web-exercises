fe980a9cf9b03536cd318b20ba41f373056638ec
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-11 11:25:09 -0500

    Update

---
+++ lecture-notes/javascript-array-lecture.js
@@ -0,0 +1,23 @@
+"use strict";
+console.log(`Link Test`);
+console.log(`---------`);
+console.log(`         `);
+
+
+var dog1 = "Fido";
+var dog2 = "Spot";
+var dog3 = "Fifi";
+var dog4 = "lola";
+
+// each item in an array is called an element
+
+var dogNames = ["Fido", "Spot", "Fifi", "Lola"];
+var dogAges = [5, 2, 3, 10];
+
+// can combine data types in a single array - avoid this
+var dogs = ["Fido", 5 "Spot", 2];
+
+// console.log(dogNames);
+console.log("Dog names: " + dogNames);
+
+console.log(dogNames.length);

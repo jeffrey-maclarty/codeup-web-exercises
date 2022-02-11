b76584ba3431956dec37c19de6b72eeaeeb2eb3a
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-11 11:16:45 -0500

    Initial

---
+++ lecture-notes/array-lecture.js
@@ -0,0 +1,13 @@
+"use strict";
+console.log(`Link Test`);
+console.log(`---------`);
+console.log(`         `);
+
+
+var dog1 = "Fido";
+var dog2 = "Spot";
+var dog3 = "Fifi";
+var dog4 = "lola";
+
+var dogs = ["Fido", "Spot", "Fifi", "Lola"];
+

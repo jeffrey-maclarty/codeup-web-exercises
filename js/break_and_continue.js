a705354a2ffc341eb8431c6115a2fdb14903e9d2
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-11 08:40:18 -0500

    Complete

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -7,42 +7,29 @@ console.log();
 console.log("Exercise Break and Continue")
 console.log("----------");
 
-// let userNum;
-//
-// console.log(`01 - userNum is ${userNum}`);
-//
-// userOdd();
+let userNum;
 
-// function userOdd() {
-//     let userNum = prompt(`Enter an odd number between 1 and 50.`);
-//     while (userNum % 2 === 0 || userNum <= 0 || userNum >= 51) {
-//
-//         alert(`${userNum} is not odd not between 1 and 50.`);
-//         console.log(`02 - userNum is ${userNum}`);
-//         break;
-//     }
-//     for (let i = 1; i <= 50; i++) {
-//         if (i === userNum) {
-//             console.log(`Skipping: ${userNum}`);
-//             continue;
-//         } else {
-//             console.log(i);
-//         }
-//     }
-    // listNumber();
-// }
-// console.log(`03 - userNum is ${userNum}`);
+userOdd();
 
-// function listNumber() {
-//     console.log(`The number to skip is _${userNum}_`);
-//     for (let i = 1; i <= 50; i++) {
-//         if (i === userNum) {
-//             console.log(`Skipping: ${userNum}`);
-//             continue;
-//         } else {
-//             console.log(i);
-//         }
-//     }
-// }
+function userOdd() {
 
+    while (true) {
+        // parseInt to use strict equality below (if i === userNum)
+        userNum = parseInt(prompt(`Enter an odd number between 1 and 50.`));
+        if (userNum % 2 !== 0 && userNum >= 1 && userNum <= 50) {
+            break;
+        }
+    }
 
+    console.log(`Number to skip is: ${userNum}`);
+    console.log();
+
+    for (var i = 1; i < 50; i += 2) {
+        if (i === userNum) {
+            console.log(`\nYikes! Skipping number: ${i}`);
+            console.log()
+            continue;
+        }
+        console.log(`Here is an odd number: ${i}`);
+    }
+}

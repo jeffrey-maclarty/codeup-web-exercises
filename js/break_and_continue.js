5fb8c551e158d89d049a34513049b03ec97ef101
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-11 10:55:40 -0500

    Added review example

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -3,9 +3,6 @@ console.log(`Link Test`);
 console.log(`---------`);
 console.log();
 
-console.log();
-console.log(`Exercise Break and Continue`)
-console.log(`----------`);
 // Prompt the user for an odd number between 1 and 50. Use a loop
 // and a break statement to continue prompting the user if they enter
 // invalid input.
@@ -37,4 +34,38 @@ function userOdd() {
         }
         console.log(`Here is an odd number: ${i}`);
     }
+}
+
+// review:
+// var oddNumber = promptforOddNumberInRange(1, 50);
+// oddNumber = parseInt(oddNumber);
+
+// printOddNumbersInRangeWithSkip(1, 50, oddNumber);
+
+function printOddNumbersInRangeWithSkip(low, high, skipNumber) {
+    // inclusive
+    for (let i = low; i <= high; i++) {
+        if (i === skipNumber || i % 2 === 0) {
+            // if i is the skip number then output something special
+            if (i === skipNumber) {
+                console.log("Yikes! Skipping number: " + i);
+            }
+            continue;
+        }
+        // if you get here then should be an odd number that is with range and not skipNumber
+        console.log("Here is an odd number: " + i);
+    }
+}
+
+function promptforOddNumberInRange(low, high) {
+    var result;
+    // loop until user gives an odd number in the range INCLUSIVE of range boundaries
+    while (true) {
+        result = prompt("Enter an odd number from " + low + " to " + high);
+        // console.log(result);
+        if(result % 2 === 1 && result >= low && result <= high) {
+            break;
+        }
+    }
+    console.log(result);
 }

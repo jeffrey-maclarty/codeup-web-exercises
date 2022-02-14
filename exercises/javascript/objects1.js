616e6c8541eccac6a3098757bcbabb2a138afa3e
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-14 09:00:12 -0500

    Initial

---
+++ exercises/javascript/objects1.js
@@ -0,0 +1,11 @@
+"use strict";
+console.log(`Link Test`);
+console.log(`---------`);
+console.log(`         `);
+
+// URL
+
+console.log(`------------`);
+console.log(`Exercise 1`);
+console.log(`------------`);
+

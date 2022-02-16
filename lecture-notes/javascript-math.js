fc53d72cf04b664cbd1cdd0f12f54c39b06b3ae7
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-16 12:11:15 -0500

    Initial

--- lecture-notes/javascript-math.js
+++ lecture-notes/javascript-math.js
@@ -3,9 +3,20 @@ console.log(`Link Test`);
 console.log(`---------`);
 console.log();
 
-// URL
+// https://java.codeup.com/javascript-i/objects/math/
 
-console.log();
-console.log(`Exercise 1`);
-console.log(`------------`);
+let randomNum = Math.random();
 
+console.log("Before multiplying: " + randomNum);
+console.log("After: " + (randomNum * (100 - 20)));
+console.log("Exclusive 1: " + (randomNum * (100 - 20) + 1));
+
+let rangedNum = Math.floor((randomNum * (100 - 20) + 1) + 20);
+console.log(rangedNum);
+
+
+console.log(Math.sqrt(16))
+console.log(Math.pow(4, 2))
+// Math.ceiling = round up
+// Math.pow = exponents
+// Math.sqrt = square root

bb8286813e407f083891023c9b2510439d2faac3
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-02-28 10:05:30 -0500

    Initial

---
+++ exercises/morning_reviews/2022-02-28.js
@@ -0,0 +1,27 @@
+"use strict";
+console.log(`Link Test`);
+console.log(`---------`);
+console.log();
+
+console.log();
+console.log(`Exercise 1`);
+console.log(`------------`);
+// Warmup: Write a function called removeAll that takes two parameters
+// One that represents an array and another that represents a value to be
+// removed from that array. When called this function should return an
+// array with all the values of the original array except the specified
+// value to be removed.
+
+// var bugs = ["mosquito", "ant", "scorpion", "ant", "ant", "mosquito", "typo", "reference error", "type error"];
+//
+// console.log(removeAll(bugs, "ant"));
+//
+// // should print
+// [
+//   'mosquito',
+//   'scorpion',
+//   'mosquito',
+//   'typo',
+//   'reference error',
+//   'type error'
+// ]

38c57b0bd441d6cd4eab9e0da6e63c90d16864ba
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-03-04 09:02:37 -0500

    Initial

---
+++ exercises/dom/dom-01.js
@@ -0,0 +1,43 @@
+"use strict";
+console.log(`Link Test`);
+console.log(`---------`);
+console.log();
+
+// https://java.codeup.com/javascript-i/bom-and-dom/
+
+
+// this code will produce a console log every second
+// when count >= max, the interval is cancelled, and the logging will stop
+
+// var count = 0;
+// var max = 10;
+// var interval = 5000; // interval time in milliseconds
+//
+// var intervalId = setInterval(function () {
+//     if (count >= max) {
+//         clearInterval(intervalId);
+//         console.log('All done');
+//     } else {
+//         count++;
+//         console.log('Repeating this line ' + count);
+//     }
+// }, interval);
+
+// 'use strict';
+//
+// var delay = 5000; // delay time in milliseconds
+//
+// var timeoutId = setTimeout(function () {
+//     alert('Here is a delayed hello!');
+// }, delay);
+
+// to cancel the timeout, you can call
+// clearTimeout(timeoutId);
+// prior to the delay expiring
+
+
+// redirect browser to google.com
+
+// window.location = 'http://www.google.com';
+
+location.reload();

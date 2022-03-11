3ba95f2288d556fd203fd62ceb4fcab310ebdb08
Jeffrey MacLarty <jeffrey.maclarty@gmail.com>
2022-03-11 11:15:44 -0500

    Initial jQuery notes and exercise 1

---
+++ js/jquery_exercises.js
@@ -0,0 +1,10 @@
+"use strict";
+
+"use strict";
+
+$(document).ready(function() {
+
+    // alert("DOM has finished loading");
+
+});
+
--- lecture-notes/jquery-01.js
+++ lecture-notes/jquery-01.js
@@ -2,5 +2,27 @@
 
 // https://java.codeup.com/jquery/
 
+// console.log($(".TableImage"));
 
+// CHANGING STYLES OR ATTRIBUTE CONTENT
+// 1. Get the thing
+// 2. Change the thing
+$(".TableImage").css("background-color", "green");
 
+//  $(".div.cotainer-fluid > p.col-12");
+//  console.log($(".div.cotainer-fluid > p.col-12"));
+let originalBackgroundColor;
+$("ul > li").hover(function () {
+    // hover-in function
+    // get original background color
+    originalBackgroundColor = $(this).css("background-color");
+    $(this).css("background-color");
+    console.log($(this).html("BACON"))
+    // set original background color
+    $(this).css("background-color", "gray");
+}, function() {
+    // reset original background-color
+    $(this).css("background-color", originalBackgroundColor)
+})
+
+console.log($(window));

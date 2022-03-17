9cd3f7d67d3dc2655eedb7cf243540af39efeac8
jeffrey-maclarty <jeffrey.maclarty@gmail.com>
2022-03-17 17:57:55 -0400

    Update

--- js/mapbox_maps_api.js
+++ js/mapbox_maps_api.js
@@ -3,10 +3,10 @@ console.log(`Link Test`);
 console.log(`---------`);
 console.log();
 
-    mapboxgl.accessToken = MAPBOX_KEY;
+mapboxgl.accessToken = MAPBOX_KEY;
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+container: 'map', // container ID
+style: 'mapbox://styles/mapbox/streets-v11', // style URL
+center: [-74.5, 40], // starting position [lng, lat]
+zoom: 9 // starting zoom
 });
